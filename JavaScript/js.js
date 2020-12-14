cdbca84b598674a2dde0126e029d90f346867719
neith <neith.altair.na@gmail.com>
2020-12-14 12:54:06 -0500

    CREATE = Folder JS, and some lines with variables

---
+++ JavaScript/js.js
@@ -0,0 +1,12 @@
+document.write("<h1>HOLA MUNDO</h1>")
+
+var unserName = 'userName'
+let nombre = 'NeithAltair'
+
+document.write('<h2>'+unserName+'<h2>')
+alert(nombre)
+
+//CONSTANTE
+const CON = 'noCambia'
+console.log(CON)
+
